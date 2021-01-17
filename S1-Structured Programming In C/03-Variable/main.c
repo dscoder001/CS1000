c929025b326bdfa0d186cbf265397965232594cb
dscoder001 <77528353+dscoder001@users.noreply.github.com>
2021-01-17 21:17:22 +0530

    update C variables

--- S1-Structured Programming In C/03-Variable/main.c
+++ S1-Structured Programming In C/03-Variable/main.c
@@ -7,6 +7,8 @@ int main()
     int characterAge = 35;
     printf("There was a old man named %s\n",characterName);
     printf("he was %d years old.\n",characterAge);
+
+    characterAge = 40;
     printf("He really liked the name %s\n",characterName);
     printf("but he did not like being %d.\n",characterAge);
 
