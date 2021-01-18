f54e431b2e15dca1a7a938c11003d77f5f4c6667
dscoder001 <77528353+dscoder001@users.noreply.github.com>
2021-01-19 03:44:52 +0530

    08

---
+++ S1-Structured Programming In C/09-UserInput/main (2).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int grade;
+    printf("Enter your :");
+    scanf("%c", &grade);
+    printf("Your grade is %c",grade);
+
+
+    return 0;
+}
---
+++ S1-Structured Programming In C/09-UserInput/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age;
+    printf("Enter your age:");
+    scanf("%d", &age);
+    printf("Your age is %d \n", age);
+
+
+    return 0;
+}
+
+
