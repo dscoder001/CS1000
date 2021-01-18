e4c582debed9fbfdb9bd39db441ca0d943f610c5
dscoder001 <77528353+dscoder001@users.noreply.github.com>
2021-01-18 16:42:27 +0530

    05

---
+++ S1-Structured Programming In C/06-WorkingWithNumbers/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("%f \n", 3.5+4 );
+    printf("%d \n", 6 / 2);
+    printf("%f \n", pow(2,3));
+    printf("%f", sqrt(36));
+    printf("%d \n", ceil(5.67));
+    printf("%f \n", floor(5.67));
+
+
+
+
+
+
+   return 0;
+}
