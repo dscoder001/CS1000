f6728ae589063e285d5a7a53ed045be0b352230b
dscoder001 <77528353+dscoder001@users.noreply.github.com>
2021-02-23 19:06:53 +0530

    Create main (3).c

---
+++ S1-Structured Programming In C/09-UserInput/main (3).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int name[20];
+    printf("Enter your name:");
+    fgets(name, 20, stdin);
+    printf("Your name is %s",name);
+
+
+    return 0;
+}
