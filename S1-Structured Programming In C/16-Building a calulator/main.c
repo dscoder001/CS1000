25384c4e2ebe2852be64efb55c8c0bc3d2f1497a
dscoder001 <77528353+dscoder001@users.noreply.github.com>
2021-05-29 19:57:14 +0530

    14 - building calculator

--- S1-Structured Programming In C/16-Building a calulator/main.c
+++ S1-Structured Programming In C/16-Building a calulator/main.c
@@ -9,22 +9,25 @@ int main(){
   char op;
 
   printf("Enter a number:");
-  scanf("%if", &num1);
-  printf("Enter a operator");
+  scanf("%lf", &num1);
+  printf("Enter a operator:");
   scanf(" %c", &op);
   printf("Enter a number:");
-  scanf("%if", &num2);
-
-  if(op == '+'){
-    print("%f", num1 + num2);
-  }else if (op == '-'){
-  printf("%f", num1 - num2);
-  }else if (op == '/'){
-  printf("%f", num1 / num2);
-  }else if (op == '*'){
-  printf("%f", num1 * num2);
+  scanf("%lf", &num2);
+  if (op == '+'){
+    printf(" %f", num1 + num2);
+  }else if(op == '-'){
+    printf(" %f", num1 - num2);
+  }else if(op == '/'){
+    printf(" %f", num1 / num2);
+  }else if(op == '*'){
+    printf(" %f", num1 * num2);
   }
-  printf("invalid operator");
+
+
+
+
+
 
 
 return 0;
