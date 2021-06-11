b3f2ba9323b15d1aa205d2d00253595e952b1900
dscoder001 <77528353+dscoder001@users.noreply.github.com>
2021-06-11 21:16:13 +0530

    for loops

---
+++ S1-Structured Programming In C/21-for loops/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    int LuckeyNumbers[] = {4,7,98,86,87,};
+    int i;
+    for (i=0; i < 5; i++){
+        printf("%d\n", LuckeyNumbers[i]);
+    }
+return 0;
+}
